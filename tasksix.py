a8c6d2b4aee352bb4c7f6d75b421ec559f83b74b
oderindesuliha <oderindesuliha@gmail.com>
2024-12-12 00:57:25 -0800

    Add files via upload

---
+++ tasksix.py
@@ -0,0 +1,11 @@
+counter1 = 1
+number = 1
+for count in range(1, 11):
+	number % 4 == 0
+	number = counter1*number
+	print(counter1," ")	
+counter2 = 1
+for count in range(5):
+	number = counter2*number
+	print(counter2," ")	
+		
